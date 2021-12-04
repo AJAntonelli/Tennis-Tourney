5e0ada61e743bec1de74e1ea17317d9e5c73dbc6
Ed Knueppel <edward.knueppel@zywave.com>
2021-12-03 19:57:12 -0600

    Tests Player.is_bye()

--- test_player.py
+++ test_player.py
@@ -3,18 +3,25 @@ import player
 import unittest
 
 class Test_Player(unittest.TestCase):
-
+    
+    #TODO move test to test_data_layer.py
     def test_io(self):
         f = open('players.json')
         players = json.load(f)
         f.close()
-        self.assertIsNotNone(players)
-
-        
+        self.assertIsNotNone(players)        
 
     def test_initialize(self):
         expectedName = "D. Medvedev"
         expectedRating = 215
         actual = player.Player(expectedName, expectedRating)
         self.assertEqual(expectedName, actual.name) 
-        self.assertEqual(expectedRating, actual.rating) 
+        self.assertEqual(expectedRating, actual.rating)
+
+    def test_is_bye_true(self):
+        p = player.Player('BYE', 0)
+        self.assertTrue(p.is_bye())
+
+    def test_is_bye_false(self):
+        p = player.Player('notbye', 0)
+        self.assertFalse(p.is_bye())
