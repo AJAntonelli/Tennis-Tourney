7f5bb9c6e4e45605741a7eaaf1642fccdd4d30f5
Ed Knueppel <edward.knueppel@zywave.com>
2021-12-03 19:58:46 -0600

    Adds comments to main.py program for readability

--- main.py
+++ main.py
@@ -53,15 +53,23 @@ bracket_data = get_tournament_bracket_data('players.json')
 
 players = map_players(bracket_data)
 
+#start round loop
 while len(players) > 1:
     print('\nRound: ', round_number)
+    
+    #start match loop
     while len(players) > 1:
-        player_name_1 = players.pop()
-        player_name_2 = players.pop()
-        print(player_name_1.name, "vs", player_name_2.name)
-        players_next_round = player_vs_bye(players_next_round, player_name_1, player_name_2)
-        win_probability(players_next_round, player_name_1, player_name_2)
+        player_one = players.pop()
+        player_two = players.pop()
+        print(player_one.name, "vs", player_two.name)
+        players_next_round = player_vs_bye(players_next_round, player_one, player_two)
+        win_probability(players_next_round, player_one, player_two)
+    #end match loop
+
+    #increment for next round
     round_number += 1
     players = players_next_round
     players_next_round = []
+#end round loop
+
 print('\nThe predicted winner of the tournament is: ', players[0].name)
