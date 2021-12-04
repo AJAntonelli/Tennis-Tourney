b8144ec824be9ffd2e9c3a7f09054533fdd02540
Ed Knueppel <edward.knueppel@zywave.com>
2021-12-03 19:52:36 -0600

    Adds is_bye method to Player class

--- player.py
+++ player.py
@@ -2,3 +2,6 @@ class Player:
     def __init__(self, name: str, rating:float) -> None:
         self.name = name
         self.rating = rating
+        
+    def is_bye(self):
+        return self.name == 'BYE'
