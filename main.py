5da43ef2e568b43aaf0bd95d03ad23d19c331060
Ed Knueppel <edward.knueppel@zywave.com>
2021-12-03 22:35:53 -0600

    The actual refactoring of prob, winner, printer

--- main.py
+++ main.py
@@ -26,24 +26,19 @@ def map_players(bracket_data: dict) -> list: #associates the name and rating to 
         players.append(player.Player(p['name'], p['rating']))
     return players
 
-#calculate win probability 
-def win_probability(players_next_round, player_name_1, player_name_2):
-    win_probability = player_name_1.rating / (player_name_1.rating + player_name_2.rating)
+def calculate_win_probability(player1, player2):
+    return player1.rating / (player1.rating + player2.rating)
+    
+def determine_winner(player1, player2, win_probability):   
     if random.random() <= win_probability:
-        
-        players_next_round.append(player_name_1)
-        print('Player One Wins!: ',player_name_1.name)
+        players_next_round.append(player1)
+        return player1
     else:
-        players_next_round.append(player_name_2)
-        print('Player Two Wins!: ', player_name_2.name)
-        
-# def player_vs_bye(players_next_round, player_name_1, player_name_2): #what if a player goes against a bye
-#     if player_name_1.name == "BYE" or player_name_2.name == "BYE":
-#         if player_name_1.name == "BYE":
-#             players_next_round.append(player_name_2)
-#         else:
-#             players_next_round.append(player_name_1)
-#     return players_next_round
+        players_next_round.append(player2)
+        return player2
+
+def print_winner(winner):
+    print(winner.name, 'wins!')
 
 #Program Start
 players_next_round = []
@@ -70,7 +65,9 @@ while len(players) > 1:
             else:
                 players_next_round.append(player_one)
         else:
-            win_probability(players_next_round, player_one, player_two)
+            wp = calculate_win_probability(player_one, player_two)
+            winner = determine_winner(player_one, player_two, wp)
+            print_winner(winner)
     #end match loop
 
     #increment for next round
